4678cbbbd32ed51edb2efa52fa86916ce0d11b50
Zahra <mirzakizahra@gmail.com>
2021-12-29 23:32:05 -0800

    create well class

---
+++ src/main/java/Well.java
@@ -0,0 +1,18 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Well extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String n=request.getParameter("userName");
+        out.print("Welcome "+n);
+    }
+}
